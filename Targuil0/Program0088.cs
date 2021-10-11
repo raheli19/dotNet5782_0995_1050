4ce19e1cb05f2e7661730075ebc827b75d3d4d4d
@tania1702 <czerniew@g.jct.ac.il>
2021-10-11 22:30:56 +0300

    Initial code+ Welcome function

---
+++ Targuil0/Program0088.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targuil0
+{
+    partial class Program
+    {
+        static partial void Welcome0088()
+        {
+            Console.WriteLine("I am also here!");
+        }
+    }
+}
