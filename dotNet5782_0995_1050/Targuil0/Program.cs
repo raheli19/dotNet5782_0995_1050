efd9cef8385eeb42959ba2a3c0ffa06bb83807be
raheli19 <rahamar@g.jct.ac.il>
2021-10-11 18:55:44 +0200

    initial code+ Welcome function

---
+++ dotNet5782_0995_1050/Targuil0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Targuil0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome2833();
+            Console.ReadKey();
+        }
+
+        private static void Welcome2833()
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
