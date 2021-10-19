c49803b637bb035a79321716cda2c8716e2eb941
raheli19 <rahamar@g.jct.ac.il>
2021-10-19 17:59:41 +0200

    modif

---
+++ DAL/Enum.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+namespace IDAL
+{
+    namespace DO
+    {
+        public enum DroneStatuses
+        {
+            free,maintenance,shipping
+        };
+        public enum WeightCategories
+        {
+            low, middle, heavy
+        };
+        public enum Priorities
+        {
+            regular,fast,emergency
+        };
+    }
+    
+ }
+
