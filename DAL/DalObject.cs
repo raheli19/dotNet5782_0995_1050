f0140b93069bb168f50e97319a7566be85c5838b
raheli19 <rahamar@g.jct.ac.il>
2021-10-21 14:38:27 +0200

    modif

---
+++ DAL/DalObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+//using IDAL.DO;
+
+
+
+namespace DalObject
+{
+    
+    public class DataSource
+    {
+
+        internal IDAL.DO.Client[] clients = new IDAL.DO.Client[100];
+
+
+        //creer les tableaux
+
+
+        internal class Config
+        {
+            internal static int IndexClient = 0;
+
+
+
+
+
+        }
+
+
+        public static void Initialize()
+        {
+
+
+
+        }
+
+
+
+
+
+
+    }
+  
+
+
+
+
+
+}
