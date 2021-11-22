c0e462dd1a0f88df12b880a67fc2bec60173c246
@tania1702 <czerniew@g.jct.ac.il>
2021-11-22 23:10:28 +0200

    hjk

---
+++ BL/IBL/BO/AlreadyExist.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace IBL.BO
+{
+    [Serializable]
+    internal class AlreadyExist : Exception
+    {
+        public AlreadyExist()
+        {
+        }
+
+        public AlreadyExist(string message) : base(message)
+        {
+        }
+
+        public AlreadyExist(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public AlreadyExist(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected AlreadyExist(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
