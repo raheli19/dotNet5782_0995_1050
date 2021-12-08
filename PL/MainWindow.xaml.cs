1ac89c9ef16fa783519c3e3a8ec6c274c3ff604e
@tania1702 <czerniew@g.jct.ac.il>
2021-12-08 20:44:29 +0200

    jarriveeeeeeeee paaaaaaaaaaaaaaaaaaazssssssssssss

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -19,7 +19,6 @@ using System.Windows.Shapes;
 // garde le sinoun apres avoir ajoute un dronert
 // dans la droneWindow une possibilité de fermer la fenetre
 // ii => vii voir que tout marche comme il se doitttt
-// ajouter le bonus dans les autres fenetres
 
 // design de toute les fenetres qui ne sont pas designées SANS FAIRE UNE TRUC MOOOOCHE NI TROP GNAGNAN
 // image croix dans error / V dans c est bon
